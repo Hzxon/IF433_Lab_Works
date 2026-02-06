dc8faf41d8f4a5bc77422ee3e89cd2f4e6239fbc
Hzxon <nathanlee912007@gmail.com>
2026-02-06 15:45:59 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_117431_JiroNathanLee/week01/GameStore.kt
+++ src/oop_117431_JiroNathanLee/week01/GameStore.kt
@@ -2,5 +2,9 @@ package oop_117431_JiroNathanLee.week01
 
 fun main() {
     val gameTitle = "Factorio"
-    val price = 300_000
+    val price = 100_000
+
+    println(calculateDiscount(price))
 }
+
+fun calculateDiscount(price: Int): Double = if (price > 500_000) price * (1 - 0.2) else price * (1 - 0.1)
