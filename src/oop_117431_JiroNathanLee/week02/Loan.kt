c486ec31a384589bf5fe041c23d5323086a60555
Hzxon <nathanlee912007@gmail.com>
2026-02-13 15:47:32 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_117431_JiroNathanLee/week02/Loan.kt
@@ -0,0 +1,9 @@
+package oop_117431_JiroNathanLee.week02
+
+class Loan (
+    val bookTitle: String,
+    var borrower: String,
+    var loanDuration: Int = 1
+) {
+    fun calculateFine() = if (loanDuration > 3) (loanDuration - 3) * 2000 else 0
+}
