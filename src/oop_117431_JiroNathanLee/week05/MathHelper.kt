60133b962d450dab38889641c6bc15b5b63348eb
Hzxon <nathanlee912007@gmail.com>
2026-03-06 22:41:19 +0700

    week05 (task 1) create MathHelper class with overloading

---
+++ src/oop_117431_JiroNathanLee/week05/MathHelper.kt
@@ -0,0 +1,7 @@
+package oop_117431_JiroNathanLee.week05
+
+class MathHelper() {
+    fun hitungLuas(sisi: Int) = sisi * sisi
+    fun hitungLuas(panjang: Int, lebar: Int) = panjang * lebar
+    fun hitungLuas(jariJari: Double) = 3.14 * jariJari * jariJari
+}
