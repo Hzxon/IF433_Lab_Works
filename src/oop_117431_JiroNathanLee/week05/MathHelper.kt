3f582783a04d6311505c069d1c300d08b10c17c4
Hzxon <nathanlee912007@gmail.com>
2026-03-06 22:50:58 +0700

    week05: (task 2) create abstract PaymentMethod

--- src/oop_117431_JiroNathanLee/week05/MathHelper.kt
+++ src/oop_117431_JiroNathanLee/week05/MathHelper.kt
@@ -1,6 +1,6 @@
 package oop_117431_JiroNathanLee.week05
 
-class MathHelper() {
+class MathHelper {
     fun hitungLuas(sisi: Int) = sisi * sisi
     fun hitungLuas(panjang: Int, lebar: Int) = panjang * lebar
     fun hitungLuas(jariJari: Double) = 3.14 * jariJari * jariJari
---
+++ src/oop_117431_JiroNathanLee/week05/PaymentMethod.kt
@@ -0,0 +1,6 @@
+package oop_117431_JiroNathanLee.week05
+
+abstract class PaymentMethod(val accountName: String) {
+
+    abstract fun processPayment(amount: Double)
+}
