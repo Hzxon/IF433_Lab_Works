027fc2f0832b100b38f54026c69d585dd80668ba
Hzxon <nathanlee912007@gmail.com>
2026-03-13 23:10:36 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_117431_JiroNathanLee/week06/SmartHomeHub.kt
@@ -0,0 +1,17 @@
+package oop_117431_JiroNathanLee.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
