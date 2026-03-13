19efd23bae30ff4054f8ab92131350cbff943a9c
Hzxon <nathanlee912007@gmail.com>
2026-03-13 23:16:06 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_117431_JiroNathanLee/week06/SmartLamp.kt
+++ src/oop_117431_JiroNathanLee/week06/SmartLamp.kt
@@ -1,6 +1,6 @@
 package oop_117431_JiroNathanLee.week06
 
-class SamrtLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
     override fun turnOn() {
         println("Lampu $name dinyalakan.")
     }
