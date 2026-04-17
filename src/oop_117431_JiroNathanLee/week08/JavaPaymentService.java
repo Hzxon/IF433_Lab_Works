1ea04b892ac8ce294ed459f3fcfd5b71d85b6e82
Hzxon <nathanlee912007@gmail.com>
2026-04-17 15:53:41 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_117431_JiroNathanLee/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_117431_JiroNathanLee.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
