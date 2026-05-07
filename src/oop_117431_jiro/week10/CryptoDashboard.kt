fdde71317525e6eae0d121dc91759539ddaffcd5
Hzxon <nathanlee912007@gmail.com>
2026-05-07 22:05:31 +0700

    week10: (task) test full generic crypto architecture

--- src/oop_117431_jiro/week10/CryptoDashboard.kt
+++ src/oop_117431_jiro/week10/CryptoDashboard.kt
@@ -1,14 +1,15 @@
 package oop_117431_jiro.week10
 
 fun main() {
-    val coinRepo = WalletRepository<Coin>()
 
+    val coinRepo = WalletRepository<Coin>()
     coinRepo.add(Coin("BTC", 0.5))
     coinRepo.add(Coin("ETH", 2.5))
     coinRepo.add(Coin("USDT", 100.0))
 
     val response = ApiResponse("200 OK", coinRepo.getAll())
-
+    println("=== COIN DASHBOARD ===")
+    println("Status: ${response.status}")
     response.data.forEach { coin ->
         println("Coin Name: ${coin.name} | Balance: ${coin.balance}")
     }
@@ -16,4 +17,9 @@ fun main() {
     val txRepo = WalletRepository<Transaction>()
     txRepo.add(Transaction("TX001", 150.0))
     txRepo.add(Transaction("TX002", 300.0))
+
+    println("\n=== TRANSACTION HISTORY ===")
+    txRepo.getAll().forEach { tx ->
+        println("ID: ${tx.id} | Amount: ${tx.amount}")
+    }
 }
